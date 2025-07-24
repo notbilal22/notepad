a82485402d0629bebfe185072799580711e0fbb5
notbilal22 <muhammadbilaltahir21@gmail.com>
2025-07-24 18:36:48 +0500

    main added

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream> 
+#include"trietree.h"
+#include"huffman.h"
+#include"console.h";
+using namespace std;
+int main()
+{
+	console cons;
+	cons.shopher();
+	system("pause");
+	return 0;
+
+}
